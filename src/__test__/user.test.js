e18e3f07ea120e5ac97859967c7b70a4ec198cd7
Vyacheslav Miroshnik <mepa087@gmail.com>
2023-03-29 20:46:30 +0300

    fix variables

--- src/__test__/user.test.js
+++ src/__test__/user.test.js
@@ -3,7 +3,7 @@ import getLevel from '../user';
 test.each([
   ['status ok', 4, 'Ваш текущий уровень: 5'],
   ['status false', '', 'Информация об уровне времменно не доступна'],
-])('check  %s', (_, id, str) => {
+])('check  %s', (_, id, level) => {
   const result = getLevel(id);
-  expect(result).toBe(str);
+  expect(result).toBe(level);
 });
